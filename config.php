45235901e527488ab8a282718a83d51b1228181c
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-19 07:59:00 +0800

    create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DSN', 'mysql:host=localhost;dbname=user_dotinstall');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'rwrwrwrw0521');
+
+define('SITE_URL', '192.168.33.10/contact/index.php');
+define('PASSEWORD_KEY', 'rwrwrwrw0521');
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+session_set_cookie_params(0, '/user/');
