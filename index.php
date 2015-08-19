53dbe4a4d72ca0e559a1cd18f2132f321c68c2b1
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-19 08:03:58 +0800

    create functions.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('config.php');
+require_once('functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>ホーム画面</title>
+	</head>
+	<body>
+		<h1>ユーザー一覧</h1>
+	</body>
+</html>
